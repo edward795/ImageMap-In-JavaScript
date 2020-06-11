561935aa770fe9453946b97099b81db74bd35af9
Ayyappadas G.B <44163764+edward795@users.noreply.github.com>
2020-06-10 22:09:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showTutorial(name) {
+               document.myform.stage.value = name
+            }
+
+            function myfunc(area){
+               alert("you clicked on "+area+" Area");
+            }
